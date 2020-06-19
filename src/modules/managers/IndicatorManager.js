9bc5274fbd472f4cb9026d46e7803c72482dca84
Ifedayo Karim <karimjnr@gmail.com>
2020-06-19 15:03:17 +0100

    feat: Resolved indicator manager

--- src/modules/managers/IndicatorManager.js
+++ src/modules/managers/IndicatorManager.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const requireAll = require('require-all');
+
 const IndicatorBuilder = require('./manager-helpers/IndicatorBuilder');
+const IndicatorPeriod = require('./manager-helpers/IndicatorPeriod');
 
 class IndicatorManager {
     constructor({candlesRepository, logger, eventEmitter, exchangeManager}) {
@@ -33,14 +35,43 @@ class IndicatorManager {
         return this.indicators; // It returns an Object
     }
 
-    async run(indicatorName, symbol, exchangeName, options) {
+    async run( indicatorName, exchangePair, options) {
         try {
+            const { symbol, exchangeName } = exchangePair;
             const theIndicator = this.find(indicatorName);
             if (!theIndicator) throw new Error(`Indicator not found`);
             const theExchange = this.exchangeManager.find(exchangeName);
             if (!theExchange) throw new Error(`Exchange not found`);
 
+            let indicatorOptions = options || theIndicator.getOptions();
+
+            const {
+                candlesRepository,
+                logger,
+                eventEmitter,
+                exchangeManager,
+            } = this;
+
+            const indicatorBuilder = new IndicatorBuilder({
+                candlesRepository,
+                exchange: theExchange,
+                eventEmitter,
+                symbol,
+                logger,
+                exchangeManager
+            })
+
+            theIndicator.buildIndicators(indicatorBuilder, indicatorOptions);
+            const builtIndicators = await indicatorBuilder.buildIndicators();
+            if (!builtIndicators) throw new Error('Error building indicators');
+
+            const indicatorPeriod = new IndicatorPeriod(this.logger);
+
+            await indicatorPeriod.setup(exchangePair, indicatorBuilder)
             
+            const indicatorResult = await theIndicator.period(indicatorPeriod, indicatorOptions);
+            
+            return indicatorResult;
             
         } catch (error) {
             this.logger.warn(`Indicator Manager: Error running [${indicatorName}:${symbol}:${exchangeName} (error.message)]`);
