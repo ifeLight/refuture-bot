2ae9f9fef2fc74cff441750940f98268c6bbd1df
Ifedayo Karim <karimjnr@gmail.com>
2020-07-17 13:44:48 +0100

    feat: added trader service

--- src/services/Trader.js
+++ src/services/Trader.js
@@ -1,3 +1,28 @@
-module.exports = class Trader {
+const StrategyManager = require('../modules/managers/StrategyManager');
 
+const eventEmitter = require('../events/EventEmitter');
+const logger = require('../utils/logger');
+// const notifier = require('../notify/index');
+
+class Trader {
+    constructor () {
+        this.logger = logger;
+        this. eventEmitter = eventEmitter;
+        this.notifier = undefined;
+    }
+
+    start(instances) {
+        try {
+            const { logger, eventEmitter, notifier } = this;
+            const strategyManager = new StrategyManager({ logger, eventEmitter, notifier })
+            instances.forEach(instance => {
+                strategyManager.add(instance);
+            });
+            strategyManager.runStrategies();
+        } catch (error) {
+            throw error;
+        }
+    }
 }
+
+module.exports = Trader;
