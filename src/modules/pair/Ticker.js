8c32218d12be5619045c97773f842becdc699b6a
Ifedayo Karim <karimjnr@gmail.com>
2020-07-26 10:48:58 +0100

    fix: convert tstring tofloat

--- src/modules/pair/Ticker.js
+++ src/modules/pair/Ticker.js
@@ -1,9 +1,9 @@
 module.exports = class Ticker {
     constructor ({bidPrice, bidQty, askPrice, askQty, lastPrice}) {
-        this.bidPrice = bidPrice;
-        this.bidQty = bidQty;
-        this.askPrice = askPrice;
-        this.askQty = askQty;
-        this.lastPrice = lastPrice
+        this.bidPrice = parseFloat(bidPrice);
+        this.bidQty = parseFloat(bidQty);
+        this.askPrice = parseFloat(askPrice);
+        this.askQty = parseFloat(askQty);
+        this.lastPrice = parseFloat(lastPrice);
     }
 }
