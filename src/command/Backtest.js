9528e2317080ef2a2ed0bfb1deef68f8d6669583
Ifedayo Karim <karimjnr@gmail.com>
2020-07-28 16:28:52 +0100

    feat: added backtest command options

---
+++ src/command/Backtest.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const fs = require('fs');
+
+const BactestService = require('../services/Backtest');
+
+module.exports = async function ({
+    configFile,
+    amount,
+    takeProfit,
+    stopLoss,
+    startDate,
+    endDate,
+    exchange,
+    symbol,
+    leverage,
+    indicator, }) {
+        const cmdBactestFilePath = configFile ? configFile : 'backtest-config-sample.json';
+        const bactestFile = path.join(process.cwd(), cmdBactestFilePath)
+        if (!fs.existsSync(bactestFile)) throw new Error(`Instance File: Instance File does not Exist (${bactestFile})`);
+        const backtestConfig = JSON.parse(fs.readFileSync(bactestFile).toString());
+        const defaultStartDate = new Date();
+        defaultStartDate.setDate(defaultStartDate.getDate() - 1);
+        
+        const parameters = {
+            amount: amount || backtestConfig.amount || 1000,
+            takeProfit: takeProfit || backtestConfig.takeProfit || 0.3,
+            stopLoss: stopLoss || backtestConfig.stopLoss || 0.2,
+            startDate: startDate || backtestConfig.startDate || defaultStartDate,
+            endDate: endDate || backtestConfig.endDate || Date.now(),
+            exchange: exchange || backtestConfig.exchange || 'binance',
+            symbol: symbol || backtestConfig.symbol || 'BTC/USDT',
+            leverage: leverage || backtestConfig.leverage || 1,
+            indicator: indicator || backtestConfig.indicator || 'bollingerSimple',
+        }
+
+        if(new Date(parameters.startDate) >= new Date(parameters.endDate)) {
+            throw new Error('Start date should be lesser than End Date')
+        }
+
+        try {
+            const backTestService = new BactestService(parameters);
+            await backTestService.start()
+        } catch (error) {
+            console.error("Backtest Service Failed while running");
+            console.error(error.message);
+        }
+}
