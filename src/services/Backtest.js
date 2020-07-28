80fe5d2e02685fdbb236ae231925acb52951c60a
Ifedayo Karim <karimjnr@gmail.com>
2020-07-28 16:32:12 +0100

    feat: started backtest

---
+++ src/services/Backtest.js
@@ -0,0 +1,14 @@
+const IndicatorManager = require('../modules/managers/IndicatorManager');
+const CandlesRepository = require('../modules/repository/CandlesRepository');
+
+class Backtest {
+    constructor(parameters) {
+
+    }
+
+    async start() {
+
+    }
+}
+
+module.exports =  Backtest;
