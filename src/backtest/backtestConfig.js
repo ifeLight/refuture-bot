124014f9308a096ca5112041d8638137cc24209e
Ifedayo Karim <karimjnr@gmail.com>
2020-07-29 00:20:06 +0100

    chores: adding bactester helpers

---
+++ src/backtest/backtestConfig.js
@@ -0,0 +1,16 @@
+module.exports = {
+	defaultBalance: 1000,
+	defaultExchangeFee: 0.002,
+	defaultStopLoss: 0.03,
+	defaultTakeProfit: 0.05,
+	positionTypes: {
+		LONG: 'long',
+		SHORT: 'short',
+		NONE: 'none',
+	},
+	orderTypes: {
+		LIMIT: 'limit',
+		MARKET: 'market',
+	},
+	defaultIndicatorPeriod: 14,
+};
