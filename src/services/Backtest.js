96dca34f8c974242898bf2acf413c8c4db08151e
Ifedayo Karim <karimjnr@gmail.com>
2020-07-28 23:11:23 +0100

    feat: added run indicator function

--- src/services/Backtest.js
+++ src/services/Backtest.js
@@ -2,9 +2,11 @@ const IndicatorManager = require('../modules/managers/IndicatorManager');
 const CandlesRepository = require('../modules/repository/CandlesRepository');
 const ExchangeManager = require('../modules/managers/ExchangeManager');
 const ExchangePair = require('../backtest/ExchangePair');
+
 const logger = require('../backtest/utils/logger');
 const eventEmitter = require('../backtest/utils/eventEmitter');
 
+const periodToTimeDiff = require('../utils/periodToTimeDiff');
 
 class Backtest {
     constructor(parameters) {
@@ -27,7 +29,116 @@ class Backtest {
     }
 
     async start() {
+        const {
+            indicator,
+            startDate,
+            endDate,
+            exchange: exchangeName,
+            symbol,
+            period,
+            orderType,
+            leverage,
+            fee
+        } = this.parameters;
+        let indicatorName, indicatorOptions;
+        let tradingFee;
 
+        // Checking Indicator
+        if (typeof indicator === 'string') {
+            indicatorName = indicator;
+        } if (typeof indicator === 'object' && indicator.name) {
+            indicatorName = indicator.name;
+            indicatorOptions = indicator.options;
+        } else {
+            throw new Error('Invalid indicator')
+        }
+
+        // Init Exchange Pair
+        try {
+            this.exchangePair.init(exchangeName, symbol);
+            await this.exchangePair.setup();
+            await this.exchangePair.setLeverage(leverage);
+            const pairInfo = this.exchangePair.info;
+            const { maker, taker } = pairInfo.fees
+            tradingFee = parseFloat(fee) || Math.max(parseFloat(maker), parseFloat(taker));
+        } catch (error) {
+           throw error; 
+        }
+
+        const exchange = this.exchangeManager.find(exchangeName);
+
+        // Fetching Candles
+        const startTime = new Date(startDate);
+        const endTime = new Date(endDate);
+        const fetchedCandles = this.candlesRepository.fetchCandlesByTimeDifference({
+            exchange,
+            symbol,
+            period,
+            startTime,
+            endTime
+        });
+
+        // Mapping candles
+        const mappedCandles = fetchedCandles.mapp((candle) => {
+            const { time, open, high, close, low, volume } = candle
+            return [time, open, high, low, close, volume];
+        })
+
+        // Creating signals
+        const signals = await this.runIndicator({
+            fetchedCandles,
+            indicatorName,
+            orderType,
+            indicatorOptions,
+            period
+        });
+
+    }
+
+    async runIndicator ({fetchedCandles, indicatorName, orderType, indicatorOptions, period}) {
+        let signals = {};
+        let lastSignal = null;
+        const periodtoTime = periodToTimeDiff(period);
+        const candlePointUnitTime = parseInt(periodtoTime / 3);
+        const isFutures = this.exchangePair.isFutures();
+
+        // Running Indicator Initial
+        await this.indicatorManager.runInit(indicatorName, this.exchangePair, indicatorOptions)
+
+        //Running Indicators For Sinals
+        for (const candle of fetchedCandles) {
+            const {open, high, low, close, time } = candle;
+            const candlePoints = [
+                {price: parseFloat(open), time: time },
+                {price: parseFloat(high), time: time + (candlePointUnitTime * 1)},
+                {price: parseFloat(low), time: time + (candlePointUnitTime * 2)}
+            ]
+
+            for (const candlePoint of candlePoints) {
+                const { time, price } = candlePoint
+                this.exchangePair.setLastSignal(lastSignal);
+                this.exchangePair.setMarkPrice(price);
+                this.exchangePair.setLastPrice(price);
+                this.exchangePair.setTime(time);
+                const signalResult = await this.indicatorManager.run(indicatorName, this.exchangePair, indicatorOptions);
+
+                if (!signalResult || (signal && !signalResult.getSignal())) {
+                    // Do nothing
+                } else if (signalResult.getSignal() && signalResult.getSignal() === 'long') {
+                    signals[time] = { positionType: 'long', orderType, price };
+                    lastSignal = 'long';
+                 } else if (signalResult.getSignal() && signalResult.getSignal() === 'short') {
+                        let positionType = isFutures ? 'short' : 'none';
+                        signals[time] = { positionType, orderType, price };
+                        lastSignal = 'short';
+                } else if (signalResult.getSignal() && signalResult.getSignal() === 'close') {
+                    signals[time] = { positionType: 'none', orderType, price };
+                    lastSignal = 'close';
+                }
+                
+            }
+        }
+        return signals;
     }
 }
 
