bdb242579c63ad7f3ebfa5200083a5ef0572bada
Ifedayo Karim <karimjnr@gmail.com>
2020-08-03 10:56:57 +0100

    feat: solved tlind indicator builder

--- src/modules/managers/manager-helpers/indicator-builder-helpers/Tulind.js
+++ src/modules/managers/manager-helpers/indicator-builder-helpers/Tulind.js
@@ -1,10 +1,12 @@
-const logger = require('../../../../utils/logger');
-const eventEmitter = require('../../../../events/EventEmitter')
+const tulind = require('tulind');
 
 module.exports = class Candles {
-    constructor({exchange, symbol}) {
-        this.exchange= exchange;
+    constructor({candlesRepository, exchange, symbol, logger, eventEmitter}) {
+        this.candlesRepository = candlesRepository;
+        this.exchange = exchange;
         this.symbol = symbol;
+        this.logger = logger;
+        this.eventEmitter = eventEmitter;
     }
 
     getName() {
@@ -12,15 +14,65 @@ module.exports = class Candles {
     }
     
     init(options) {
-        this.period = options.period
+        this.candlesLength = options.length;
+        this.options = options;
+        this.name = options.name;
+        this.indicatorOptions = options.options;
+
+    }
+
+    insideBuild(name, inputs, options) {
+        return new Promise((resolve, reject) => {
+            tulind.indicators[name].indicator(inputs, options, function(err, results) {
+                if (err) {
+                    reject(err);
+                    return;
+                }
+                resolve(results);
+              });
+        });
     }
 
     async build() {
         try {
-            // TODO Will be fixed later
-            throw new Error('Still on progress');
+            const { exchange, symbol, period, candlesLength, name, indicatorOptions} = this;
+            const tulindIndicator = tulind.indicators[name];
+            if (!tulindIndicator) throw new Error(`No tulind indicator with name: ${name}`);
+            const length = parseInt(candlesLength);
+            const candles = await this.candlesRepository.fetchCandlesByNumberFromNow({exchange, symbol, period, length});
+            if (!candles) throw new Error('Unable to fetch candles')
+            let res = {};
+            res.close = candles.map(candle => candle.close);
+            res.open = candles.map(candle => candle.open);
+            res.low = candles.map(candle => candle.low);
+            res.high = candles.map(candle => candle.high);
+            res.volume = candles.map(candle => candle.volume);
+
+            let indicatorInputs = []
+            let indicatorResolvedOptions = [];
+            if (tulindIndicator.inputs && tulindIndicator.inputs > 0) {
+                for (const inputName of tulindIndicator['input_names']) {
+                    if (inputName == 'real') {
+                        indicatorInputs.push(res.close);
+                        continue;
+                    }
+                    indicatorInputs.push(res[inputName]);
+                }
+            }
+            
+            if (tulindIndicator.options && tulindIndicator.options > 0) {
+                for (let optionName of tulindIndicator['option_names']) {
+                    const splittedOptionName = optionName.split(' ');
+                    optionName = splittedOptionName.length  < 3 ? splittedOptionName[0] : splittedOptionName[1];
+                    indicatorResolvedOptions.push(indicatorOptions[optionName]);
+                }
+            }
+
+            const results = await this.insideBuild(name, indicatorInputs, indicatorResolvedOptions);
+            return results;
+
         } catch (error) {
-            this.logger(`Tulind Builder: indicator builder Failed [${this.symbol}] (${error.message})`)
+            this.logger.warn(`Tulind Builder: indicator builder Failed [${this.symbol}] (${error.message})`)
         }
     }
 }
