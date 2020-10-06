00765f87590b5bc0218088dfc836e8cbebb8bbe1
Ifedayo Karim <karimjnr@gmail.com>
2020-10-06 20:39:11 +0100

    feat: added lightweight chart package

---
+++ src/app/src/views/backtest/BacktestList.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>Started</h1> 
+</template>
+
+<script>
+export default {
+    data: function () {
+        return {
+            
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
