b49f8367427210f9660de2a9af649cd4e9ff8199
Ifedayo Karim <karimjnr@gmail.com>
2020-10-07 09:06:09 +0100

    feat: added gracefully app shutdown

--- src/services/app.js
+++ src/services/app.js
@@ -1,7 +1,8 @@
-const express = require('express');        // call express
-const app = express();                 // define our app using express
+const express = require('express'); // call express
+const app = express(); // define our app using express
 const bodyParser = require('body-parser');
 const config = require('config');
+const GracefulShutdownManager = require('@moebius/http-graceful-shutdown').GracefulShutdownManager;
 
 const http = require('http');
 
@@ -9,14 +10,18 @@ const api = require('../api/index')
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(bodyParser.json());
 
-const port = config.get('app.port') || 8100;        // set our port
+const port = config.get('app.port') || 8100; // set our port
 
 // test route to make sure everything is working (accessed at GET http://localhost:8100/api)
 app.get('/', function (req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });
+    res.json({
+        message: 'hooray! welcome to our api!'
+    });
 });
 
 // more routes for our API will happen here
@@ -27,6 +32,17 @@ app.use('/api', api);
 
 // START THE SERVER
 // =============================================================================
-http.createServer(app).listen(port, function (){
+const server = http.createServer(app).listen(port, function () {
     console.log('Trade App Server started  on port ' + port);
 })
+
+const shutdownManager = new GracefulShutdownManager(server);
+
+const shutdownServer = () => {
+    shutdownManager.terminate(() => {
+        console.log('App Server is gracefully terminated');
+    });
+}
+
+process.on('SIGTERM', shutdownServer);
+process.on('SIGINT', shutdownServer);
