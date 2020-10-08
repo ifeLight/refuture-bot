ad7c0ab4ccae6a60497d48ebe0a4206289bf9cb9
Ifedayo Karim <karimjnr@gmail.com>
2020-10-08 16:10:56 +0100

    chores: finalised huper single page

--- src/app/src/views/backtest/BacktestSingle.vue
+++ src/app/src/views/backtest/BacktestSingle.vue
@@ -5,11 +5,49 @@
 </template>
 
 <script>
-export default {
 
+export default {
+    mounted () {
+        const id = this.$route.params.id;
+        if (!id) {
+            this.notifyError('Error Fetching Data - No ID given');
+            return;
+        }
+        this.id = id;
+        
+    },
+    data () {
+        return {
+            id :null,
+        }
+    },
+    methods: {
+        notifyError(message) {
+            this.$vs.notify({icon: 'error', title: "Error", color: 'danger', text: message})
+        },
+        openLoading () {
+          this.$vs.loading();
+      },
+      closeLoading () {
+          this.$vs.loading.close();
+      },
+        fetchBacktest(id) {
+            this.openLoading();
+            this.$http.get(`/api/backtest/${id}`)
+            .then((response) => {
+                this.closeLoading();
+                const data = response.data;
+
+            })
+            .catch((err) => {
+                this.closeLoading();
+            })
+        }
+    }
 }
 </script>
 
 <style>
+
 
 </style>
